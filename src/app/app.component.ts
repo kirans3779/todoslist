c1e14d801a4b352d623f0c226305cb455c6f7333
kiran.sonwane <kiran.sonwane@cmssl.in>
2023-09-27 23:09:24 +0530

    code is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { Sort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todos-list';
+export class AppComponent implements OnInit{
+
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns = ['id', 'title', 'userId','completed'];
+
+  constructor(private http: HttpClient) {}
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+  ngOnInit(): void {
+    // Fetch data from the API
+    this.http.get('https://jsonplaceholder.typicode.com/todos').subscribe((data: any) => {
+      this.dataSource.data = data;
+      console.log(this.dataSource.data);
+      
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  sortData(sort: Sort) {
+    const data = this.dataSource.data;
+    if (!sort.active || sort.direction === '') {
+      this.dataSource.data = data;
+      return;
+    }
+
+    this.dataSource.data = data.sort((a,b):any => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'id':
+          return compare(a.id, b.id, isAsc);
+        case 'userid':
+          return compare(a.userid, b.userId, isAsc);
+        case 'title':
+          return compare(a.title, b.title, isAsc);
+        case 'completed':
+          return compare(a.completed, b.completed, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
+  // Toggle task completion
+  onTaskCompleted(task:any): void {
+    task.completed = !task.completed;
+  }
 }
+function compare(name: any, name1: any, isAsc: boolean) {
+  throw new Error('Function not implemented.');
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {NgFor} from '@angular/common';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    FormsModule,
+    HttpClientModule,
+    MatCheckboxModule,
+    NgFor,
+    MatPaginatorModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
